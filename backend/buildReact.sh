55de3b23c49ac71387bbf3eb47ce0b638b86d757
mmc-servel <svelescu@gmail.com>
2022-04-21 12:24:53 +0300

    Improve UI code

--- backend/buildReact.sh
+++ backend/buildReact.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# pkill -f backend
+
 cd $1
 npm run build
 
--- frontend/src/components/LogArea.js
+++ frontend/src/components/LogArea.js
@@ -4,10 +4,8 @@ import classes from "./LogArea.module.css";
 
 function LogArea(props) {
 
-  const [email,setEmail] = useState('');
-  props.solution.onLogAreaRefresh=setEmail;
-  props.solution.username=email; //TO DO: why it doesn't work without this?
-  console.log("propsUser",props.solution.username);
+  const [dummy,setDummy] = useState('');
+  props.solution.onLogAreaRefresh=setDummy;
 
   return (
     <div className={classes.internal_login_div}>
